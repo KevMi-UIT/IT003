f30040ef2b771ff524c9a61babefbed78786727e
Leminionn <137601647+Leminionn@users.noreply.github.com>
2024-04-02 20:46:24 +0700

    Ass 3 Bai_7 he-he

--- Assignment_3/7-Reverse_linked_list.cpp
+++ Assignment_3/7-Reverse_linked_list.cpp
@@ -28,21 +28,6 @@ class SinglyLinkedList
         this->tail = nullptr;
     }
 };
-void print_singly_linked_list(SinglyLinkedListNode *node, char sep = ' ')
-{
-    while (node)
-    {
-        cout << node->data;
-
-        node = node->next;
-
-        if (node)
-        {
-            cout << sep;
-        }
-    }
-    cout << '\n';
-}
 
 // Complete the insertSortedLinkedList function below.
 
@@ -55,20 +40,15 @@ void print_singly_linked_list(SinglyLinkedListNode *node, char sep = ' ')
  * };
  *
  */
-void print_singly_linked_list(SinglyLinkedListNode *node, char sep = ' ')
+void printLinkedList(SinglyLinkedList *list)
 {
-    while (node)
+    SinglyLinkedListNode *p;
+    p = list->head;
+    while (p != NULL)
     {
-        cout << node->data;
-
-        node = node->next;
-
-        if (node)
-        {
-            cout << sep;
-        }
+        cout << p->data << " ";
+        p = p->next;
     }
-    cout << '\n';
 }
 void insert_node(SinglyLinkedList *List, int x)
 {
@@ -86,9 +66,31 @@ void insert_node(SinglyLinkedList *List, int x)
     List->tail = node;
 }
 
-void reverseLinkedList(SinglyLinkedList List)
+void reverseLinkedList(SinglyLinkedList *List)
 {
-    SinglyLinkedList *temp = new SinglyLinkedList;
+    int count = 0;
+    SinglyLinkedListNode *p1 = List->head;
+    SinglyLinkedListNode *p2 = p1;
+    SinglyLinkedListNode *p3 = p1;
+
+    while (p1)
+    {
+        count++;
+        p1 = p1->next;
+    }
+
+    int *tempList = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        tempList[i] = p2->data;
+        p2 = p2->next;
+    }
+
+    for (int i = count - 1; i >= 0; i--)
+    {
+        p3->data = tempList[i];
+        p3 = p3->next;
+    }
 }
 
 int main()
